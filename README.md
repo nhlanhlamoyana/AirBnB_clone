77c07cb48f964268be9926b129d675433935241b
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-03-12 20:56:09 -0700

    Console 0.0.1

---
+++ README.md
@@ -0,0 +1,6 @@
+The AirBnB clone project is to deploy a simple copy of the AirBnB website on my server.
+Each task of the AirBnB clone project is linked and will enable me to create a simple flow of serialization/deserialization.To put in place a parent class called(BaseModel)to take care of initialization of future instances.To create the first abstracted storage engine of the project called file storage. To lastly create all unittests to authenticate all classes and storage engine.
+
+
+A command interpreter is similar to the Shell project with limited to specific uses such as a command interpreter can create new objects such as a new User or a new Place. It can also retrieve an object from a file or database.It can update attributes of an object and destroy an object.
+
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Defines unittests for models/city.py.
+
+
+Unittest classes:
+    TestCity_instantiation
+    TestCity_save
+    TestCity_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from mode;s.city import City
+
+
+class TestCity_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the City class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(City, type(City()))
+
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(City(), models.storage.all().values())
+
+    
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(City().id))
+
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().created_at))
+
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().updated_at))
+
+
+    def test_state_id_is_public_class_attribute(self):
+        cy = City()
+        self.assertEqual(str, type(City.state_id))
+        self.assertIn("state_id", dir(cy))
+        self.assertNotIn("state_id", cy.__dict__)
+
+
+    def test_name_is_public_class_attribute(self):
+        cy = City()
+        self.assertEqual(str, type(City.name))
+        self.assertIn("name", dir(cy))
+        self.assertNotIn("name", cy.__dict__)
+
+
+    def test_two_cities_unique_ids(self):
+        cy1 = City()
+        cy2 = City()
+        self.assertNotEqual(cy1.id, cy2.id)
+
+
+    def test_two_cities_different_created_at(self):
+        cy1 = City()
+        sleep(0.05)
+        cy2 = City()
+        self.assertLess(cy1.created_at, cy2.created_at)
+
+
+    def test_two_cities_different_updated_at(self):
+        cy1 = City()
+        sleep(0.05)
+        cy2 = City()
+        self.assertLess(cy1.updated_at, cy2.updated_at)
